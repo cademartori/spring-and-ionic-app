a49f6c14f0c3ab1e481bf7bda7d77d1727256a47
Wesley Cademartori <wesleycademartori@hotmail.com>
2020-06-07 15:40:54 -0300

    Lista de categorias com DTO

--- src/main/java/com/cademartori/cursomc/services/CategoriaService.java
+++ src/main/java/com/cademartori/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.cademartori.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +40,11 @@ public class CategoriaService {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 				throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos!!");		
-}
+		}
 		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
